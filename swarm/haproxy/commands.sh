92c8899c900284da7e894a9f58cf02592d09bac5
nvsec <32085007+nrvguitarist@users.noreply.github.com>
2021-06-05 00:22:34 -0400

    Update commands.sh

--- swarm/haproxy/commands.sh
+++ swarm/haproxy/commands.sh
@@ -1,13 +1,13 @@
 # create the haproxy service
 docker service create \
 --name ha \
---constraint node.hostname==udock \
 --network elastic \
 --mode replicated \
 --replicas 2 \
 --endpoint-mode vip \
 --update-parallelism 1 \
 --update-delay 60s \
+--replicas-max-per-node 1
 --publish published=80,target=80,protocol=tcp,mode=ingress \
 --publish published=443,target=443,protocol=tcp,mode=ingress \
 --publish published=6514,target=6514,protocol=tcp,mode=ingress \
@@ -17,3 +17,6 @@ haproxytech/haproxy-debian:2.0
 
 # hopefully don't need this with Dockerfile adding this.
 --mount type=bind,src=/etc/haproxy/,dst=/etc/haproxy/,ro=true \
+
+#deprecated in favor of 2 replicas and max per node
+--constraint node.hostname==udock \
