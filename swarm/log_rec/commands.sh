7c42bd37605855b1171deaf56a3266c9cef8ed2a
nvsec <32085007+nrvguitarist@users.noreply.github.com>
2021-06-05 00:24:21 -0400

    Update commands.sh

--- swarm/log_rec/commands.sh
+++ swarm/log_rec/commands.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-docker build -t log_rec:1.1 .
+docker build -t log_rec:1.2 .
 
 # create the log_rec service
 docker service create \
 --name log_rec \
---constraint node.hostname==udock \
 --network elastic \
 --mode replicated \
---replicas 1 \
+--replicas 2 \
 --endpoint-mode dnsrr \
 --update-parallelism 1 \
 --update-delay 60s \
-log_rec:1.1
+log_rec:1.2
