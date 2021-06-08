15dc149aaf1cec48f51a386c42957477c3e10951
nvsec <32085007+nrvguitarist@users.noreply.github.com>
2021-06-07 23:01:01 -0400

    Update commands.sh

--- swarm/redis/commands.sh
+++ swarm/redis/commands.sh
@@ -4,3 +4,13 @@ docker push udock.sharpt:8443/red:1.0.0
 
 docker service update red --image udock.sharpt:8443/red:1.0.0
 
+# create the redis service
+docker service create \
+--name red \
+--network elastic \
+--mode replicated \
+--replicas 2 \
+--endpoint-mode dnsrr \
+--update-parallelism 1 \
+--update-delay 60s \
+udock.sharpt:8443/red:1.0.0
