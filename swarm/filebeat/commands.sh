dcd5dd136a1622dec4d0eba3fa4f7cb822a0834c
nvsec <32085007+nrvguitarist@users.noreply.github.com>
2021-06-08 08:02:57 -0400

    Update commands.sh

--- swarm/filebeat/commands.sh
+++ swarm/filebeat/commands.sh
@@ -1,7 +1,7 @@
-docker build -t fb9516_redis:1.0.5 .
-docker image tag fb9516_redis:1.0.5 udock.sharpt:8443/fb9516_redis:1.0.5
-docker push udock.sharpt:8443/fb9516_redis:1.0.5
-docker service update fb --image udock.sharpt:8443/fb9516_redis:1.0.5
+docker build -t fb9516_redis:1.0.6 .
+docker image tag fb9516_redis:1.0.6 udock.sharpt:8443/fb9516_redis:1.0.6
+docker push udock.sharpt:8443/fb9516_redis:1.0.6
+docker service update fb --image udock.sharpt:8443/fb9516_redis:1.0.6
 
 # create the filebeat service
 docker service create \
@@ -12,5 +12,5 @@ docker service create \
 --endpoint-mode dnsrr \
 --update-parallelism 1 \
 --update-delay 60s \
-udock.sharpt:8443/fb9516_redis:1.0.5
+udock.sharpt:8443/fb9516_redis:1.0.6
 
