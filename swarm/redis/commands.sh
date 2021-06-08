d4ca2f4722cbc650d800632110b777a49aaa0a56
nvsec <32085007+nrvguitarist@users.noreply.github.com>
2021-06-07 22:58:43 -0400

    Update commands.sh

--- swarm/redis/commands.sh
+++ swarm/redis/commands.sh
@@ -1,1 +1,6 @@
+docker build -t red:1.0.0 .
+docker image tag red:1.0.0 udock.sharpt:8443/red:1.0.0
+docker push udock.sharpt:8443/red:1.0.0
+
+docker service update red --image udock.sharpt:8443/red:1.0.0
 
