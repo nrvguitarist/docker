366f625e1c13771b2fc6068201a4d4839a64e69c
nvsec <32085007+nrvguitarist@users.noreply.github.com>
2021-06-07 20:51:45 -0400

    Update commands.sh

--- swarm/filebeat/commands.sh
+++ swarm/filebeat/commands.sh
@@ -1,1 +1,13 @@
+docker build -t fb9516_redis:1.0.0 .
+
+# create the filebeat service
+docker service create \
+--name fb \
+--network elastic \
+--mode replicated \
+--replicas 2 \
+--endpoint-mode dnsrr \
+--update-parallelism 1 \
+--update-delay 60s \
+fb9516_redis:1.0.0
 
