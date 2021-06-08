5929d3cc6e56dc1106fb246d5c6b4df1df6dd916
nvsec <32085007+nrvguitarist@users.noreply.github.com>
2021-06-07 23:12:51 -0400

    Update commands.sh

--- swarm/haproxy/commands.sh
+++ swarm/haproxy/commands.sh
@@ -1,4 +1,8 @@
 docker build -t hap:1.0 .
+docker image tag hap:1.0 udock.sharpt:8443/hap:1.0
+docker push udock.sharpt:8443/hap:1.0
+
+docker service update hap --image udock.sharpt:8443/hap:1.0
 
 # create the haproxy service
 docker service create \
@@ -9,14 +13,14 @@ docker service create \
 --endpoint-mode vip \
 --update-parallelism 1 \
 --update-delay 60s \
---replicas-max-per-node 1
+--replicas-max-per-node 1 \
 --publish published=80,target=80,protocol=tcp,mode=ingress \
 --publish published=443,target=443,protocol=tcp,mode=ingress \
 --publish published=6514,target=6514,protocol=tcp,mode=ingress \
 --publish published=5601,target=5601,protocol=tcp,mode=ingress \
 --publish published=9200,target=9200,protocol=tcp,mode=ingress \
---publish published=6379,target=6379,protocol=tcp,mode=ingress \
-hap:1.0
+--publish published=9516,target=9516,protocol=tcp,mode=ingress \
+udock.sharpt:8443/hap:1.0
 
 # hopefully don't need this with Dockerfile adding this.
 --mount type=bind,src=/etc/haproxy/,dst=/etc/haproxy/,ro=true \
